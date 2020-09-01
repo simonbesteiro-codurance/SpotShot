8cd117261e4514b219eb1d305f008d53275e31bc
Simón Fernández Besteiro <simonbesteiro@gmail.com>
2020-09-01 11:49:50 +0200

    finish spot component teting

--- frontend/src/test/spotStore.spec.js
+++ frontend/src/test/spotStore.spec.js
@@ -12,27 +12,50 @@ function reduceAction(action, state) {
 describe("Spot Component", () => {
   let spotStoreCallback;
   let action;
+  let id;
+  let myObject;
+  beforeAll(() => {
+    id = 1;
+    myObject = [{ id: 0, image: "myImage", description: "myDescription" }];
+  });
   beforeEach(() => {
     spotStoreCallback = jest.fn();
     spotStore.addChangeListener(spotStoreCallback);
-    action = reduceAction(actionTypes.LOAD_SPOT, [
-      { id: 0, image: "myImage", description: "myDescription" },
-    ]);
-
+    action = reduceAction(actionTypes.LOAD_SPOT, myObject);
     dispatcher.dispatch(action);
   });
   afterEach(() => {
     spotStore.removeChangeListener(spotStoreCallback);
   });
-  xit("should emit a change", () => {
-    expect(spotStore.emitChange()).toHaveBeenCalled();
+
+  it("should emit a change without error", () => {
+    expect(spotStore.emitChange).toBeTruthy();
   });
-  xit("should receive a spot giving an id", () => {
-    const id = 1;
-    expect(spotStore.getSpotById(id)).toHaveBeenCalled();
+  it("should add change listener without error", () => {
+    expect(spotStore.addChangeListener).toBeTruthy();
   });
+  it("should remove change listener  without error", () => {
+    expect(spotStore.removeChangeListener).toBeTruthy();
+  });
+
+  it("should receive a spot giving an id", () => {
+    id = 1;
+    expect(spotStore.getSpotById(id)).toBeDefined();
+  });
+  it("should receive undefined giving an not valid id", () => {
+    id = 0;
+    expect(spotStore.getSpotById(id)).not.toBeDefined();
+  });
+
   it("should return a spot list", () => {
-    const id = 1;
-    expect(spotStore.getSpot()).toHaveBeenCalled();
+    expect(spotStore.getSpot()).toBe(myObject);
+  });
+
+  it("should enter in default with a not valid Action", () => {
+    action = reduceAction(actionTypes.MYACTION, [
+      { id: 0, image: "myImage", description: "myDescription" },
+    ]);
+    dispatcher.dispatch(action);
+    expect(spotStore).toBeTruthy();
   });
 });
