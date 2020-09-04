7d3ae75c8d1270121b2d902931eb0cb7a77f2241
Simón Fernández Besteiro <simonbesteiro@gmail.com>
2020-09-04 11:41:24 +0200

    init map view

--- frontend/src/components/Map.jsx
+++ frontend/src/components/Map.jsx
@@ -1,10 +1,46 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { Dimensions, Image } from "react-native";
+import MapView, { Marker } from "react-native-maps";
 
 export default function MAp() {
   return (
     <>
-      <Text>Map works</Text>
+      <MapView
+        style={{
+          flex: 1,
+          height: Dimensions.get("window").height,
+          width: Dimensions.get("window").width,
+        }}
+      >
+        <Marker
+          coordinate={{ latitude: 42.23552, longitude: 2.12002 }}
+          title={"Test"}
+          description={"Showld draw spotShot"}
+        >
+          <Image
+            source={require("../Images/SpotShotLogo.png")}
+            style={{
+              height: Dimensions.get("window").height * 0.1,
+              width: Dimensions.get("window").width * 0.1,
+              resizeMode: "contain",
+            }}
+          />
+        </Marker>
+        <Marker
+          coordinate={{ latitude: 41.53552, longitude: 2.12002 }}
+          title={"Test"}
+          description={"Showld draw spotShot"}
+        >
+          <Image
+            source={require("../Images/SpotShotLogo.png")}
+            style={{
+              height: Dimensions.get("window").height * 0.1,
+              width: Dimensions.get("window").width * 0.1,
+              resizeMode: "contain",
+            }}
+          />
+        </Marker>
+      </MapView>
     </>
   );
 }
