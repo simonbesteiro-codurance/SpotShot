ab3a5f786733161c7dfb3503d4a5d6f96392f5f7
Simón Fernández Besteiro <simonbesteiro@gmail.com>
2020-09-06 17:28:12 +0200

    fix typo

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,4 +1,4 @@
-,import React, { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { TextInput, View, Button, Text } from "react-native";
 import stylesLogin from "../styles/login-style";
 
