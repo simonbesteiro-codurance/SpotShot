5c078d6d2ee06f8bea00221e0eb3265504d8fe3c
Simón Fernández Besteiro <simonbesteiro@gmail.com>
2020-09-16 10:04:42 +0200

    spot route Controller test

--- backend/src/__test__/spotRoutecontroller.spec.js
+++ backend/src/__test__/spotRoutecontroller.spec.js
@@ -1,33 +1,102 @@
 const { expect } = require("chai");
-
+const databaseCalls = require("../database-utils/databaseCalls");
 const sinon = require("sinon");
 const spotRouteController = require("../controller/spotRouteController");
+const uploadImageServiceLogic = require("../controller/uploadImageServiceLogic");
+const { get } = require("mongoose");
 
-describe("Spot Route Controller test", () => {
+describe("Spot Route Controller Test", () => {
   let req;
   let res;
-  let controller;
-  beforeEach(() => {
+  let response;
+  afterEach(() => {
+    sinon.restore();
+  });
+  it("should get an spot by id when spotId exist", (done) => {
+    response = "myResponse";
+    req = {
+      params: {
+        spotId: "mySpotId",
+      },
+    };
+    res = {
+      json: () => {},
+    };
+    sinon.stub(databaseCalls, "getProductById").returns(
+      new Promise((resolve) => {
+        resolve(response);
+      })
+    );
+    const jsonSpy = sinon.spy(res, "json");
+    spotRouteController.get(req, res);
+    done();
+    expect(jsonSpy.called).to.be.true;
+  });
+  it("should get all spots when spotId does not exist", (done) => {
+    response = "myResponse";
     req = {
       params: {},
     };
     res = {
       json: () => {},
     };
-    controller = spotRouteController;
-  });
-  xit("should return all spots", () => {
+    sinon.stub(databaseCalls, "getAllProducts").returns(
+      new Promise((resolve) => {
+        resolve(response);
+      })
+    );
     const jsonSpy = sinon.spy(res, "json");
-    controller.get(req, res);
+    spotRouteController.get(req, res);
+    done();
     expect(jsonSpy.called).to.be.true;
   });
-  xit("should return a spot by id", () => {
+  it("should delete an spot giving an id", (done) => {
+    response = "myResponse";
     req = {
-      params: { spotId: "5f4e4766174ddd4c09fabc9f" },
+      params: {},
+      body: {
+        spotId: "mySpotId",
+      },
     };
-
-    const jsonSpy = sinon.spy(res, "json");
-    controller.get(req, res);
-    expect(jsonSpy.called).to.be.true;
+    res = {
+      json: () => {},
+      send: () => {},
+    };
+    sinon.stub(databaseCalls, "removeSpotById").returns(
+      new Promise((resolve) => {
+        resolve(response);
+      })
+    );
+    const sendSpy = sinon.spy(res, "send");
+    spotRouteController.deleter(req, res);
+    done();
+    expect(sendSpy.called).to.be.true;
+  });
+  it("should upload an Image", (done) => {
+    response = "myResponse";
+    req = {
+      params: {},
+      body: {
+        spotId: "mySpotId",
+      },
+      headers: {},
+    };
+    res = {
+      json: () => {},
+      send: () => {},
+      status: () => {},
+    };
+    sinon.stub(databaseCalls, "removeSpotById").returns(
+      new Promise((resolve) => {
+        resolve(response);
+      })
+    );
+    const Busboy = (params) => {
+      this.on = (arg,callback) => ;
+    };
+    const sendSpy = sinon.spy(res, "send");
+    spotRouteController.uploadImage(req, res);
+    done();
+    expect(sendSpy.called).to.be.true;
   });
 });
